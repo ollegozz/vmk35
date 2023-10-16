f447b9f3f73738f54b6a234303a1e9394d2bb44f
Олег <OllegOzz@yandex.ru>
2023-10-16 17:28:57 +0300

    pages

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,11 @@ import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Login from "../pages/Login/Login";
 import Main from "../pages/Main/Main";
 import About from "../pages/About/About";
+import Catalog from "../pages/Catalog/Catalog";
+import News from "../pages/News/News";
+import SalesPoints from "../pages/SalesPoints/SalesPoints";
+import Partners from "../pages/Partners/Partners";
+import Contacts from "../pages/Contacts/Contacts";
 
 
 export const privateRoutes = [
